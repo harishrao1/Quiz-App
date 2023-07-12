8e510ab43bbccd06278108dff921895933d44a97
Harishrao <harishrao1846@gmail.com>
2023-07-12 15:48:21 +0530

    updated

--- config.js
+++ config.js
@@ -5,8 +5,8 @@ const generateJwtSecret = () => {
 };
 console.log("JSWTOKEN", generateJwtSecret());
 module.exports = {
-  port: process.env.PORT || 3000, // Using the PORT environment variable if set, otherwise use 3000
-  dbURI: process.env.DB_URI || "mongodb://127.0.0.1:27017/quiz", // Using the DB_URI environment variable if set, otherwise  the local MongoDB URI
-  jwtSecret: process.env.JWT_SECRET || generateJwtSecret(), // Using the JWT_SECRET environment variable if set, otherwise generating a new secret key
+  port: process.env.PORT || 3000,
+  dbURI: process.env.DB_URI || "mongodb://127.0.0.1:27017/quiz",
+  jwtSecret: process.env.JWT_SECRET || generateJwtSecret(),
   jwtExpiresIn: process.env.JWT_SECRET,
 };
